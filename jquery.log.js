63ba5f2c113142c9dc52d04a627ff9ea8f9ea6db
Adam Cuppy <acuppy@gmail.com>
2011-11-23 00:59:18 -0800

    better docs and bug fixes
    Signed-off-by: Adam Cuppy <acuppy@gmail.com>

--- jquery.log.js
+++ jquery.log.js
@@ -1,7 +1,7 @@
 /**
  * jquery.log.js
  * 
- * @version 0.1.0
+ * @version 0.2.1
  * @author Adam Cuppy 
  * @copyright http://defineyouredge.com
  * 
@@ -30,21 +30,39 @@
 			// replaces console.log with alert popup
 			windows_safe: false
 		},
+		
+		/**
+		 * Log level conversion
+		 */
+		levels : {
+			'info'  : 0,
+			'debug' : 1,
+			'warn'  : 2,
+			'error' : 3
+		},
 	
-		// initialized the plugin and logs the message
-		log: function( options, obj ) {
+		/**
+		 * initialized the plugin and logs the message
+		 * 
+		 * @param {Mixed} options : hash of configuration options or a String which is the messages
+		 * @param {String} log_level : of this message only
+		 * @param {Object} obj : associated jQuery object
+		 * @return void
+		 */
+		log: function( options, log_level, obj ) {
 			
-			// private writer
-			//
-			// @msg: string output to log
-			// @obj: object to attach to the log
-			function write_to_console( msg, obj ){
-				if( Logger.settings.active ){
+			var _s = Logger.settings;
+			
+			function write_to_console( msg, level, obj ){
+				if( _s.active ){
 					
-					if( Logger.settings.windows_safe ){
+					if( _s.windows_safe ){
 						alert( msg );
-					} else {
-						switch( Logger.settings.log_level ){
+						return;
+					}
+					
+					if ( Logger.levels[level] <= Logger.levels[_s.log_level] ) {
+						switch( level ){
 							case 'info':
 								console.info("%s: %o", msg, obj);
 								break
@@ -66,8 +84,8 @@
 				}
 			}
 			
-			if( Logger.settings.windows_safe ){
-				if( Logger.settings.collapsed ){
+			if( !_s.windows_safe ){
+				if( _s.collapsed ){
 					console.groupCollapsed('Console log for: "'+ obj.selector+'"');
 				} else {
 					console.group('Console log for: "'+ obj.selector+'"');
@@ -77,67 +95,75 @@
 			collection = obj.each(function(){
 				switch( typeof options ){
 					case 'string':
-						write_to_console( options, this);
+						write_to_console( options, log_level, this);
 						break
 				  case 'function':
-				  	write_to_console( options( this ), this);
+				  	write_to_console( options( this ), log_level, this);
 				  	break
 				  case 'object':
-				  	$.extend( Logger.settings, options );
+				  	$.extend( _s, options );
 				  	break
 				  case 'boolean':
-				  	Logger.settings.active = options;
+				  	_s.active = options;
 				  	break
 				  default:
 				  	$.error('Unsupported parameter passed to logger');
 				};
 			});
 			
-			if( Logger.settings.windows_safe ) console.groupEnd();
-			if( Logger.settings.backtrace )    console.trace();
+			if( _s.windows_safe ) console.groupEnd();
+			if( _s.backtrace )    console.trace();
 			
 			return collection;
   	} // EOF Logger.init
   }; // EOF Logger
   
-  // Simple log writer and Logger configuration setter
-  //
-  // @options mixed:
-  //			string : Output directly to the console.log
-  // 			function : Process a callback function to return a string to output to the console.log
-  // 			object: Pass configuration parameters to Logger.settings for all future log messages
-  //		 	boolean: Pass true/false to Logger.settings.active to activate/deactivate the logger
-  //
-  // @log_level string: 
-  //		  Only works when options is a String or Callback function
-  //			Set the log level for output of that instance
-  //
-  $.log = function( options, log_level ){
-		switch( typeof options ){
-			case 'string':
-				if( typeof log_level == 'string') Logger.settings.log_level = log_level;
-				Logger.log( options, $(document) );
-				break
-		  case 'function':
-		  	if( typeof log_level == 'string') Logger.settings.log_level = log_level;
-		  	Logger.log( options(), $(document) );
-		  	break
-		  case 'object':
-		  	$.extend( Logger.settings, options );
-		  	break
-		  case 'boolean':
-		  	Logger.settings.active = options;
-		  	break
-		  default:
-		  	$.error('Unsupported parameter passed to logger');
-		};
+  /**
+   * Shorthand logging method
+   * 
+   * @param {Mixed} options : mixed use
+   * @return void
+   */
+  $.log = function( options ){
+  	try {
+  		var _s = Logger.settings;
+  		
+	  	if( typeof arguments[1] != 'string' ) log_level = 'info';
+	  			
+			switch( typeof options ){
+				case 'string':
+					Logger.log( options, log_level, $(document) );
+					break
+			  case 'function':
+			  	Logger.log( options(), log_level, $(document) );
+			  	break
+			  case 'object':
+			  	$.extend( _s, options );
+			  	break
+			  case 'boolean':
+			  	_s.active = options;
+			  	break
+			  default:
+			  	$.error('Unsupported parameter passed to logger');
+			};
+		} catch(err) {
+			console.log('Logger had the following error: '+ err.toString());
+		}
 	};
 	
-	// Primary chainable jQuery constructor
-	// 
-	$.fn.log = function( msg, log_level ){
-		if( typeof log_level == 'string') $.log({ log_level: log_level });		
-		Logger.log( msg, this);
+	/**
+	 * Initiates the chain-ability of the plugin
+	 * 
+	 * @param {Mixed} msg : string log message OR callback to initiate to make a log entry
+	 * @param {String} log_level : string representing the log_level
+	 * @return {jQuery} passthru of the original object
+	 */
+	$.fn.log = function( msg, log_level, scope ){
+		try {	
+			Logger.log( msg, log_level, scope || this);
+		} catch(err) {
+			console.log('Logger had the following error: '+ err.toString());
+		}
 		return this;
 	}
   
